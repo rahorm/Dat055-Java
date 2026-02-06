4242e59e793e45c5902ea8ee55e96386018b0fbd
andy <andrea.axring@gmail.com>
2026-02-06 16:38:05 +0100

    added javaDoc to Controller and included javaDoc for ChatRoomFacade in the end

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,22 +1,71 @@
 package Controller;
 
-import Model.ChatRoomModel;
-
+/**
+ * Notes:
+ * - Change UML name to controller
+ * - where is all the dif chatrooms stored?, method 1 needs it maybe?
+ * - should msgs and user params be of type user and message?
+ * - i think numberformatexception is wrong
+ * */
 public class Controller {
 
-    public void setActiveRoom(int chatID){
+    /**
+     * Takes an integer representing an existing chatRoom ID and makes that the active chatRoom
+     * @param chatID integer representing a chatRoom
+     * @throws IllegalArgumentException if chatID isn't an existing ID for a chatRoom
+     * */
+    public void changeActiveRoom(int chatID){
 
     }
 
-    public void sendMessage(String message){
+    /**
+     * Takes message and stores it in the program
+     * @param msg string that represents a message that has been input from a user
+     * @param userID integer value that represents a user in the active chat that is the sender of the message
+     * @throws IllegalArgumentException if userID is not an existing user
+     * @throws NumberFormatException if msg isn't a string*/
+    public void sendMessage(String msg, int userID){
+
+    }
+    /**
+     * Removes the chatRoom, you can not remove the activeChatRoom
+     * @param chatID integer that represents a chatRoom
+     * @throws IllegalArgumentException if chatID does not represent a chatRoom that exists
+     * @throws IllegalArgumentException if the chaTID given is the activeChatRoom
+     * */
+    public void removeChatRoom(int chatID){
 
     }
 
-    public void removeChatRoom(){
-
-    }
-
+    /**
+     * Creates a new ChatRoom and makes that the activeChatRoom
+     * @throws IllegalStateException if the max amount of chatRooms have been reached
+     * */
     public void addChatRoom(){
 
     }
+
+    /**
+     * */
+    public void addObserver(){
+
+    }
+
+    public void removeObserver(){
+
+    }
+
+    public void notifyObservers(){
+
+    }
+
+    /**
+     * Takes an integer representing an existing chatRoom ID and makes that the active chatRoom
+     * @param chatID integer representing a chatRoom
+     * @throws IllegalArgumentException if chatID isn't an existing ID for a chatRoom
+     * */
+    public void changeActiveRoom(int chatID){
+
+    }
+
 }
