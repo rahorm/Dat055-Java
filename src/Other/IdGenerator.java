f2288054590b1559bc93fbae3d36c52b3e6cb223
Madde <mcelarsson@gmail.com>
2026-02-10 14:06:58 +0100

    updated javadoc for idGen

--- src/Other/IdGenerator.java
+++ src/Other/IdGenerator.java
@@ -3,10 +3,12 @@ package Other;
 public class IdGenerator {
     /**
      * Generates an id
-     * Type specifies if the id is for a chat, user or message. Use 'c' for chat, 'u' for user, 'm' for message.
+     * Type specifies if the id is for a chat, user or message. Use 'c' for chat, 'u' for user, 'm' for message, 'p' for picture.
      *
      * @param type
      * @return int generated id
+     *
+     * @throws IllegalArgumentException if type isn't specified
      */
 
     int generateId(char type){
