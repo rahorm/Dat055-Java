439fb0cd5e3a09052bcebc53984cdc3e846739b9
Ada Berggren <linus.pg.berggren@gmail.com>
2026-02-10 15:20:46 +0100

    documented view

--- src/View/UI.java
+++ src/View/UI.java
@@ -1,4 +1,10 @@
 package View;
 
+import javax.swing.*;
+
 public class UI {
+    public static void init() {
+        JButton btn = new JButton();
+        btn.addActionListener((_) -> System.out.println("hallo"));
+    }
 }
