a8e3874076566e7ae7c074599733b8fdcf51456b
bokchoi <beatrootxbokchoi@gmail.com>
2026-02-13 14:02:45 +0100

    ModelFacade added javadoc/methods

--- src/Model/ChatRoomFacade.java
+++ src/Model/ChatRoomFacade.java
@@ -5,24 +5,28 @@ import java.util.Observable;
 public class ChatRoomFacade extends Observable {
     private ChatRoomModel model;
 
-    public void addObserver(){
 
-    }
-
-    public void removeObserver(){
-
-    }
-
-    public void notifyObservers(){
-
+    public ChatRoomFacade(ChatRoomModel model) {
+        this.model = model;
     }
 
     /**
-     * Takes an integer representing an existing chatRoom ID and makes that the active chatRoom
-     * @param chatID integer representing a chatRoom
-     * @throws IllegalArgumentException if chatID isn't an existing ID for a chatRoom
-     * */
-    public void changeActiveRoom(int chatID){
+     * User wants to change an active room, the chatroom that user wants to
+     * open will be identified by chatID, this chatID should be sent as a parameter
+     * when it's called in Control.
+     *
+     * @param chatID of a chatroom that user wants to open
+     */
+    public void changeActiveRoom(int chatID) {
+        model.changeActiveRoom(chatID);
 
+        setChanged(); // Apparently it should be used before using notifyObservers
+
+        notifyObservers(Object o);
     }
+
+		 /* Not needed now but can be useful to have/know
+		 public ChatRoomModel getState(){
+			 return this.model;
+    } */
 }
