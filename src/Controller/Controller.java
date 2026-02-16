8a0a3b9bae00a56d122a4e581006163c5f42adb8
andy <andrea.axring@gmail.com>
2026-02-16 15:34:50 +0100

    notes

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -28,6 +28,7 @@ public class Controller {
      * @throws NumberFormatException if msg isn't a string
      * */
     public void sendMessage(String msg, int userID, LocalDateTime timeStamp){
+        modelF.StoreMsg();
 
     }
     /**
