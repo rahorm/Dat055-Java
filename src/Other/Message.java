190c5f4f7e81d98b1baf27cd8b07b0128b399159
Madde <mcelarsson@gmail.com>
2026-02-16 13:42:37 +0100

    added basic constructor, implements serializable

--- src/Other/Message.java
+++ src/Other/Message.java
@@ -1,11 +1,18 @@
 package Other;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Message {
+public class Message implements Serializable {
     public String content;
     public LocalDateTime timestamp;
     public User sender;
     public int chatID;
 
+    public Message(User sender, int chatID, String content){
+        this.content = content;
+        this.sender = sender;
+        this.chatID = chatID;
+        this.timestamp = LocalDateTime.now();
+    }
 }
