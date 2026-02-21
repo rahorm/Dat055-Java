9b14e98e80b82f3a6fb1e432a6da9f429d698efd
MCEHL <115644997+MCEHL@users.noreply.github.com>
2026-02-21 12:39:50 +0100

    Added queries for easier resetting

--- tables.sql
+++ tables.sql
@@ -1,21 +1,39 @@
+/*******************************************************************************************
+To reset the database:
+In this file, run all queries in step 1, this deletes all the tables
+In this file, run all queries in step 2, this creates all tables again
+In file inserts.sql, run all queries in the file, this fills all the tables with test data
+*******************************************************************************************/
+
+-- STEP 1 --
+DROP TABLE ChatMessages;
+DROP TABLE ChatMembers;
+DROP TABLE Users;
+DROP TABLE Chats;
+
+-- Step 2 --
 CREATE TABLE Users(
     username TEXT,
     pswd TEXT NOT NULL,
-    PRIMARY KEY (username) ON UPDATE CASCADE
+    PRIMARY KEY (username)
 ); 
 
 CREATE TABLE Chats(
     chatId INT,
     name TEXT NOT NULL,
-    PRIMARY KEY (chatId) ON UPDATE CASCADE
+    PRIMARY KEY (chatId)
 );
 
 CREATE TABLE ChatMembers(
     chat INT,
     member TEXT,
     PRIMARY KEY (chat, member),
-    FOREIGN KEY (chat) REFERENCES Chats(chatId) ON DELETE CASCADE,
-    FOREIGN KEY (member) REFERENCES Users(username) ON DELETE CASCADE
+    FOREIGN KEY (chat) REFERENCES Chats(chatId)
+        ON DELETE CASCADE
+         ON UPDATE CASCADE,
+    FOREIGN KEY (member) REFERENCES Users(username)
+        ON DELETE CASCADE
+         ON UPDATE CASCADE
 );
 
 CREATE TABLE ChatMessages(
@@ -26,9 +44,15 @@ CREATE TABLE ChatMessages(
     content TEXT NOT NULL, --kan det vara null?, text can förvara upp till 65535 chars ~typ 20 sidor
     hasImg BOOLEAN NOT NULL,
     PRIMARY KEY (msgId),
-    FOREIGN KEY (chat) REFERENCES Chats(chatId) ON DELETE CASCADE,
-    FOREIGN KEY (sender) REFERENCES Users(username) ON DELETE CASCADE,
-    FOREIGN KEY (chat, sender) REFERENCES ChatMembers(chat, member) ON DELETE CASCADE
+    FOREIGN KEY (chat) REFERENCES Chats(chatId)
+        ON DELETE CASCADE
+         ON UPDATE CASCADE,
+    FOREIGN KEY (sender) REFERENCES Users(username)
+        ON DELETE CASCADE
+         ON UPDATE CASCADE,
+    FOREIGN KEY (chat, sender) REFERENCES ChatMembers(chat, member)
+        ON DELETE CASCADE
+         ON UPDATE CASCADE
 );
 
 ------- Inte färdig ---------------
