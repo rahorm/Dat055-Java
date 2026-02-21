c7ab01a8f3c65a4fa907eeb11a9f8590beaaf6d5
MCEHL <115644997+MCEHL@users.noreply.github.com>
2026-02-21 12:08:17 +0100

    Made upadates so you actually can delete stuff

--- tables.sql
+++ tables.sql
@@ -1,21 +1,21 @@
 CREATE TABLE Users(
     username TEXT,
     pswd TEXT NOT NULL,
-    PRIMARY KEY (username)
-);
+    PRIMARY KEY (username) ON UPDATE CASCADE
+); 
 
 CREATE TABLE Chats(
     chatId INT,
     name TEXT NOT NULL,
-    PRIMARY KEY (chatId)
+    PRIMARY KEY (chatId) ON UPDATE CASCADE
 );
 
 CREATE TABLE ChatMembers(
     chat INT,
     member TEXT,
     PRIMARY KEY (chat, member),
-    FOREIGN KEY (chat) REFERENCES Chats(chatId),
-    FOREIGN KEY (member) REFERENCES Users(username)
+    FOREIGN KEY (chat) REFERENCES Chats(chatId) ON DELETE CASCADE,
+    FOREIGN KEY (member) REFERENCES Users(username) ON DELETE CASCADE
 );
 
 CREATE TABLE ChatMessages(
@@ -23,12 +23,12 @@ CREATE TABLE ChatMessages(
     chat INT NOT NULL,
     sender TEXT NOT NULL,
     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    content TEXT NOT NULL, --kan det vara null, text can förvara upp till 65535 chars ~typ 20 sidor
+    content TEXT NOT NULL, --kan det vara null?, text can förvara upp till 65535 chars ~typ 20 sidor
     hasImg BOOLEAN NOT NULL,
     PRIMARY KEY (msgId),
-    FOREIGN KEY (chat) REFERENCES Chats(chatId),
-    FOREIGN KEY (sender) REFERENCES Users(username),
-    FOREIGN KEY (chat, sender) REFERENCES ChatMembers(chat, member)
+    FOREIGN KEY (chat) REFERENCES Chats(chatId) ON DELETE CASCADE,
+    FOREIGN KEY (sender) REFERENCES Users(username) ON DELETE CASCADE,
+    FOREIGN KEY (chat, sender) REFERENCES ChatMembers(chat, member) ON DELETE CASCADE
 );
 
 ------- Inte färdig ---------------
