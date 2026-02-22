3b194a5d08086d2a37b4c83f0f178ea324457dc8
andy <andrea.axring@gmail.com>
2026-02-22 12:56:28 +0100

    Request type edit

--- src/Common/RequestType.java
+++ src/Common/RequestType.java
@@ -1,8 +1,11 @@
 package Common;
 
 public enum RequestType {
+    // chatroom requests
     ADD_CHATROOM, DELETE_CHATROOM, GET_CHAT_MEMBERS, GET_AVAILABLE_CHATS, ADD_CHAT_MEMBER, REMOVE_CHAT_MEMBER,
+    // user requests
     ADD_USER, DELETE_USER, GET_USERS, CHECK_LOGIN,
+    // msg requests
     ADD_MESSAGE, DELETE_MESSAGE, GET_MESSAGES, EDIT_MESSAGE,
 }
 
