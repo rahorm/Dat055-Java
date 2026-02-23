1698ad0a0c64aa8fcd699ea0aee8956f47adbe02
Ada Berggren <linus.pg.berggren@gmail.com>
2026-02-23 15:57:59 +0100

    layouting

--- src/View/UI.java
+++ src/View/UI.java
@@ -10,30 +10,73 @@ import java.util.ArrayList;
 
 public class UI {
     Controller controller;
-    JLabel messages;
+    JTextArea messages;
+    JTextArea messageInput;
+    JTextField usernameInput;
+    JPasswordField passwordInput;
 
     public UI(Controller controller) {
-        this.messages = new JLabel();
+        this.messages = new JTextArea();
+        messages.setEditable(false);
+        messages.setColumns(20);
+        messages.setRows(10);
+
+
         this.controller = controller;
-        LayoutManager layout = new GridLayout(1, 1);
+
+        this.messageInput = new JTextArea();
+        messageInput.setColumns(20);
+        messageInput.setRows(5);
+
+        this.usernameInput = new JTextField();
+        usernameInput.setSize(100, 30);
+
+        this.passwordInput = new JPasswordField();
+        passwordInput.setSize(100, 30);
+
+        SpringLayout layout = new SpringLayout();
 
         JFrame frame = new JFrame();
+        Container contentPane = frame.getContentPane();
 
+        //testknapp
         JButton btn = new JButton();
         btn.addActionListener((_) -> System.out.println("hallo"));
         btn.setSize(100, 100);
 
-        JButton btn2 = new JButton("create new chat room");
-        btn2.addActionListener((_) -> controller.sendMessage("hello server", "User1"));
+        //skickar ett meddelande till servern
+        JButton btn2 = new JButton("send message");
+        btn2.addActionListener((_) -> controller.sendMessage(messageInput.getText(), "User1"));
         btn2.setSize(100, 100);
 
-        frame.add(btn);
-        frame.add(btn2);
-        frame.add(messages);
 
+        //lägger till knappar till ui:n
+        //frame.add(btn);
+        contentPane.add(btn2);
+        contentPane.add(messages);
+        contentPane.add(messageInput);
+        //frame.add(usernameInput);
+        //frame.add(passwordInput);
+
+        //constraints för springlayout
+
+        //position messageInput
+        layout.putConstraint("East", messageInput, 10, "East", contentPane);
+
+        //position "Send Message" button
+        layout.putConstraint("North", btn2, 10, "South", messageInput);
+        layout.putConstraint("South", btn2, 10, "South", contentPane);
+        layout.putConstraint("East", btn2, 10, "East", contentPane);
+
+        //position message
+        layout.putConstraint("North", messageInput, 10, "South", messages);
+        //layout.putConstraint("South", messages, 10, "North", messageInput);
+        layout.putConstraint("East", messages, 10, "East", contentPane);
+
+        //setup för fönstret
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(layout);
+        contentPane.setLayout(layout);
         frame.setVisible(true);
     }
 
