2371cdd93b01d4d2b452e888e60af3064c1aeb7a
Ada Berggren <linus.pg.berggren@gmail.com>
2026-03-04 15:02:34 +0100

    displays image

--- src/View/MessageRenderer.java
+++ src/View/MessageRenderer.java
@@ -1,6 +1,7 @@
 package View;
 
 import Other.Message;
+import Other.PictureMessage;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,6 +10,7 @@ class MessageRenderer extends JPanel implements ListCellRenderer<Message> {
     private final JLabel sender = new JLabel();
     private final JLabel messageContent = new JLabel();
     private final JLabel messageTime = new JLabel();
+    private final JLabel imageContent = new JLabel();
     MessageRenderer() {
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
@@ -16,6 +18,7 @@ class MessageRenderer extends JPanel implements ListCellRenderer<Message> {
         topPanel.add(messageTime);
         setLayout(new BorderLayout(8, 0));
         add(topPanel, BorderLayout.NORTH);
+        add(imageContent, BorderLayout.CENTER);
         add(messageContent, BorderLayout.SOUTH);
         setBorder(BorderFactory.createEmptyBorder(4, 8, 4, 8));
         messageContent.setFont(messageContent.getFont().deriveFont(Font.PLAIN, 11f));
@@ -26,6 +29,10 @@ class MessageRenderer extends JPanel implements ListCellRenderer<Message> {
         sender.setText(message.getSender());
         messageContent.setText(message.getContent());
         messageTime.setText(message.getTimestamp().toString());
+        if (message instanceof PictureMessage) {
+            Icon icon = new ImageIcon(((PictureMessage) message).getImageBytes());
+            imageContent.setIcon(icon);
+        }
         if (isSelected) {
             setBackground(list.getSelectionBackground());
             setForeground(list.getSelectionForeground());
