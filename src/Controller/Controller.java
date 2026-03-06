667f2732f75578164cfef6d942a4339eb9012616
Madde <mcelarsson@gmail.com>
2026-03-06 22:30:34 +0100

    administration for controller  package

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -26,28 +26,27 @@ public class Controller {
     }
 
     /**
-     * Takes message and stores it in the program
-     * @param msg string that represents a message that has been input from a user
-     * @throws IllegalArgumentException if userID is not an existing user
-     * @throws NumberFormatException if msg isn't a string
-     * */
+     * Takes message and stores it in the model
+     * @param msg string that contains the message that should be stored
+     * @throws IllegalArgumentException if message is null or empty
+     */
     public void sendMessage(String msg){
-        //System.out.println("message entered controller");
+
         if (msg == null || msg.isEmpty()) {
             throw new IllegalArgumentException("Message cannot be null or empty");
         }
         facade.storeMsg(msg);
-        //System.out.println("message left controller");
     }
 
     /**
      * Takes a picture message and stores it in the program
      * @param imagePath string that represents the local file path of the image to be sent
+     * @param message string that contains the message that should be stored
      * @throws IllegalArgumentException if imagePath or user is null/empty
+     * @throws IllegalArgumentException if message is null
      * @throws IllegalArgumentException if the file at imagePath does not exist
      */
     public void sendPictureMessage(String imagePath, String message) {
-        //System.out.println("picture message entered controller");
 
         if (imagePath == null || imagePath.isEmpty()) {
             throw new IllegalArgumentException("Image path cannot be null or empty");
@@ -65,20 +64,18 @@ public class Controller {
 
         facade.storePictureMsg(pictureMessage);
 
-        //System.out.println("picture message left controller");
     }
 
     /**
-     * Removes the chatRoom, you can not remove the activeChatRoom
-     * @param chatID integer that represents a chatRoom
-     * @throws IllegalArgumentException if chatID does not represent a chatRoom that exists
-     * @throws IllegalArgumentException if the chatID given is the activeChatRoom
+     * Removes the specified chatRoom, including all messages, you can not remove the activeChatRoom
+     * @param chatID id of chatRoom to be removed
+     * @throws IllegalArgumentException if chatID does not represent a chatRoom that can exist
      * */
     public void removeChatRoom(int chatID) {
         if (chatID < 0) {
             throw new IllegalArgumentException("Invalid chatID");
         }
-        // assuming facade/model will check active room constraint
+
         facade.removeChatRoom(chatID);
     }
 
@@ -91,33 +88,18 @@ public class Controller {
     }
 
     /**
-     * sets the active user to the input id
+     * changes active user to the specified user
      *
-     * @param user String that represents id of a user in the system
-     * @throws IllegalArgumentException if id is not associated with an id that exists in the system
+     * @param user username of user
      * */
 
     public void changeActiveUser(String user) {
         facade.setActiveUser(user);
-        //facade.addMember(user); //Den här borde väl inte ha något med addMember att göra?
     }
 
 
     public void login(String user, String password){
         facade.logIn(user, password);
-        /*ReturnCode code;
-
-        if(!facade.checkUser(user)){
-            code = ReturnCode.USER_NOT_EXIST;
-        }
-
-        if(!facade.logIn(user, password)){
-            code = ReturnCode.INCORRECT_PASSWORD;
-        }
-
-        code = ReturnCode.LOGIN_SUCCESSFUL;
-
-        return code;*/
     }
 
     public void addMember(String user){
@@ -126,11 +108,12 @@ public class Controller {
     public void addMember(String user, int chatID){
         facade.addMember(user, chatID);
     }
+
     /**
-     * Creates a new user of the type User
+     * Adds a new user to the database
      *
-     * @param user String input that will be the users displayed name, not identifier in the system
-     * @throws IllegalArgumentException if input is not a string
+     * @param user will be the users displayed name and identifier in the system
+     * @throws IllegalArgumentException if username is null or empty
      * */
     public void signUp(String user, String password) {
         if (user == null || user.isEmpty()) {
