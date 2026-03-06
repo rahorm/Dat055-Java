1e799c80b7928f618f79bfc134c3a39cb5f8dd06
andy <andrea.axring@gmail.com>
2026-03-06 16:28:27 +0100

    button fix

--- src/View/UI.java
+++ src/View/UI.java
@@ -83,8 +83,13 @@ public class UI {
         loginBtn.addActionListener((_) -> controller.login(usernameInput.getText(), passwordInput.getText()));
 
         JButton changeRoomBtn = new JButton("change room");
-        changeRoomBtn.addActionListener((_) -> controller.changeActiveRoom(Integer.parseInt(roomInput.getText())));
-
+//        changeRoomBtn.addActionListener((_) -> controller.changeActiveRoom(Integer.parseInt(roomInput.getText())));
+        changeRoomBtn.addActionListener((_) -> {
+            String input = roomInput.getText().trim();
+            if (!input.isEmpty()) {
+                controller.changeActiveRoom(Integer.parseInt(input));
+            }
+        });
 
         JButton removeChatBtn = new JButton("remove chat");
 //      removeChatBtn.addActionListener((_) -> controller.removeChatRoom(Integer.parseInt(removeChatInput.getText())));
