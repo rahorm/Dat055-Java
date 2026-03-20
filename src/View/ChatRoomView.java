b177b9db5266cdc62cc512abc537eab9ee2f94ee
andy <andrea.axring@gmail.com>
2026-03-20 13:32:15 +0100

    java doc in ChatRoomView

--- src/View/ChatRoomView.java
+++ src/View/ChatRoomView.java
@@ -12,19 +12,25 @@ public class ChatRoomView implements Observer {
     Controller controller;
     ChatRoomFacade model;
     UI ui;
+
+    public ChatRoomView(Controller controller, ChatRoomFacade model) {
+        this.controller = controller;
+        this.model = model;
+        this.ui = new UI(controller);
+        model.addObserver(this);
+        update(model, null);
+    }
+
     /**
      * Used by an observable running notifyObservers() or notifyObservers(Object arg)
-     * @param o
-     * @param arg String representing what has changed in the model
-     *            "user" when the active user has changed
-     *            "message" when a new message has arrived
-     *            when arg isn't specified everything in the view gets updated
+     * @param o Represents which observer is used as an Observable object, s not used in this version.
+     * @param arg String representing what has changed in the model, is not used in this version. Can be set to 0.
      */
     public void update(Observable o, Object arg) {
         //messages
         ArrayList<Message> msg = model.getMSGList();
         ui.setMsg(msg);
-        //  available chats
+        //available chats
         ArrayList<String> chatNames = model.getAvailableChatNames();
         ArrayList<Integer> chatIds = model.getAvailableChatIds();
         ui.setAvailableChats(chatNames, chatIds);
@@ -34,14 +40,5 @@ public class ChatRoomView implements Observer {
             ui.displayStatusMessage(statusMessage);
         }
         ui.setActiveChat(model.getActiveChatRoomName());
-    }
-
-    public ChatRoomView(Controller controller, ChatRoomFacade model) {
-        this.controller = controller;
-        this.model = model;
-        this.ui = new UI(controller);
-        model.addObserver(this);
-        update(model, null);
-
     }
 }
